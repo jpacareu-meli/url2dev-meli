3244c6211cd6e59529f9a92a36e0e40e6c6345c5
Javier Pacareu <javier.pacareu@mercadolibre.cl>
2022-10-13 14:29:20 -0300

    feat: initial commit
    feat: initial commit
    
    feat: initial commit
    Update README.md
    Update README.md
    feat: add storage

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # url2dev-meli
 Chrome extension to switch URL from dev to prod and viceversa
+
+![image](https://user-images.githubusercontent.com/57413670/195655542-16573e8d-99b8-498e-b805-6d4155be08ab.png)
+
+## How to use
+Press `Switch`: https://www.mercadolibre.cl/ ➡ https://dev.mercadolibre.cl:8443/
+
+Press `Switch`: https://dev.mercadolibre.cl:8443/ ➡️ https://www.mercadolibre.cl/
+
+
+## How to Install
+
+![Alt Text](/how-to.gif)
---
+++ script.js
@@ -0,0 +1,67 @@
+function init() {
+  const btn = document.querySelector("button");
+  const port = document.querySelector("#port");
+  const subdomainProd = document.querySelector("#subdomain-prod");
+  const subdomainDev = document.querySelector("#subdomain-dev");
+
+  const onOpen = async () => {
+    const portVal = await getValue("port");
+    const subProdVal = await getValue("subdomainProd");
+    const subDevVal = await getValue("subdomainDev");
+    port.value = portVal !== undefined ? portVal : port.value;
+    subdomainProd.value =
+      subProdVal !== undefined ? subProdVal : subdomainProd.value;
+    subdomainDev.value =
+      subDevVal !== undefined ? subDevVal : subdomainDev.value;
+  };
+
+  const eventHandler = () => {
+    chrome.tabs.query(
+      { active: true, currentWindow: true },
+      async function ([activeTab]) {
+        const matchBeforeDot = /([^\.]+)/;
+        const activeURL = new URL(activeTab.url);
+        if (activeURL.port === "8443") {
+          activeURL.port = "";
+          activeURL.host = activeURL.host.replace(
+            matchBeforeDot,
+            subdomainProd.value
+          );
+        } else {
+          activeURL.port = port.value;
+          activeURL.host = activeURL.host.replace(
+            matchBeforeDot,
+            subdomainDev.value
+          );
+        }
+        await setValue("port", port.value);
+        await setValue("subdomainProd", subdomainProd.value);
+        await setValue("subdomainDev", subdomainDev.value);
+        chrome.tabs.update(activeTab.id, {
+          url: activeURL.toString(),
+        });
+      }
+    );
+  };
+
+  btn && btn.addEventListener("click", eventHandler);
+  onOpen();
+}
+
+function setValue(key, value) {
+  return new Promise((resolve) => {
+    chrome.storage.sync.set({ [key]: value }, function () {
+      resolve(value);
+    });
+  });
+}
+
+function getValue(key) {
+  return new Promise((resolve) => {
+    chrome.storage.sync.get([key], function (result) {
+      resolve(result[key]);
+    });
+  });
+}
+
+init();
